0210dfd58a9708d6181c8d185d113c36491f1fad
TawanPhiranan <65011212119@msu.ac.th>
2024-08-23 22:09:04 +0700

    login agin

--- app.ts
+++ app.ts
@@ -1,8 +1,12 @@
 import express from "express";
 import cors from "cors";
 import { router as user } from "./api/user";
+import bodyParser from "body-parser";
 
 export const app = express();
+
+app.use(bodyParser.text());
+app.use(bodyParser.json());
 // app.use("/", (req, res) => {
 //   res.send("Hello World!!! ei");
 // });
